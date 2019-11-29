a52de6191a7ebc69e56688e4e2eb91c9a32664e3
blossun <dnsngu@gmail.com>
2019-11-30 00:14:44 +0900

    2단계 요구사항 2: 시합하기 기능 구현
    메뉴 선택 3: 시합 시작 추가
    Inning 클래스명 Attack으로 변경
    Game 클래스
     - 게임에 참여하는 회초:팀1, 회말:팀2 에 대한 정보 저장
     - startGame() : 총 6회  경기 진행
     - inning() : 각 회초/ 회말별 팀의 공격진행
     - updateScore() : 공격이 끝난 후 팀의 성적 업데이트
     - printResult() : 경기 종료 후, 경기결과 화면에 표시
    if 문에 잘못 쓴 () 삭제

--- baseball.py
+++ baseball.py
@@ -13,9 +13,11 @@ class Team: #팀 정보
     def __init__(self,team_name):
         self.team_name = team_name
         self.player_list = []
+        self.score = 0 #득점
+        self.current_player = '' #현재 타자
 
     def inputInfo(self): #선수 데이터 입력
-        for i in range(9): 
+        for i in range(9):
             print(str(i+1)+'번 타자 정보 입력> ')
             name, ba = sys.stdin.readline().rstrip().split(' ')
             self.player_list.append( Player(i,name,ba) );
@@ -29,6 +31,51 @@ class Team: #팀 정보
         print()
 
 class Game:
+    def __init__(self,team1, team2):
+        self.top = team1 #회초
+        self.bottom = team2 #회말
+
+    def startGame(self):
+        print(self.top.team_name +' VS ' + self.bottom.team_name +'의 시합을 시작합니다.')
+        for i in range(6):
+            print("+++++++++++++++++++++++++++++++++++++++")
+            print(str(i+1)+'회초 '+self.top.team_name+' 공격')
+            result = self.inning(self.top)
+            self.updateScore(result,self.top)
+            print("+++++++++++++++++++++++++++++++++++++++")
+            print(str(i+1)+'회말 '+self.bottom.team_name+' 공격')
+            result = self.inning(self.bottom)
+            self.updateScore(result,self.bottom)
+        self.printResult()
+
+    def inning(self,team):
+        round = Attack() #공격결과 hits count만 가져와서 득점 추가?
+        while(True):
+            round.throw()
+            round.updateResult()
+            if(round.outs == 3): #3아웃이면 전체 안타수 출력 후 경기 종료
+                round.display()
+                print('최종 안타수: {}\n GAME OVER'.format(round.hits))
+                return round.hits
+                # break;
+            if(round.result == 'hits' or round.result == 'outs'):
+                print('다음 타자가 타석에 입장했습니다.')
+                round.strike = 0
+                round.ball = 0
+            round.display()
+    def updateScore(self,result,team):
+        if result >= 4:
+            team.score += result-2
+    def printResult(self):
+        print("=======================================")
+        print("경기 종료")
+        print(self.top.team_name +' VS ' + self.bottom.team_name)
+        print(str(self.top.score)+' : '+str(self.bottom.score))
+        print("Thank you")
+        print("=======================================")
+
+
+class Attack: #Inning->attack ?
     def __init__(self):
         self.strike = 0
         self.ball = 0
@@ -42,46 +89,46 @@ class Game:
         #self.result =result_list[math.floor(random.random()*len(result_list))]
         self.result = random.choice(result_list)
     def updateResult(self):
-        if(self.result == 'strike'):
+        if self.result == 'strike':
             self.strike += 1
             print("스트라이크!")
-            if(self.strike == 3): # 3 strike => 1 outs
+            if self.strike == 3: # 3 strike => 1 outs
                 print("3 스트라이크~", end=' ')
                 self.result = 'outs'
                 self.strike = 0
-        if(self.result == 'ball'):
+        if self.result == 'ball':
             self.ball += 1
             print("볼!")
-            if(self.ball == 4): #4 ball => 1 hits
+            if self.ball == 4: #4 ball => 1 hits
                 print("4볼~", end=' ')
                 self.result = 'hits'
                 self.ball = 0
-        if(self.result == 'outs'):
+        if self.result == 'outs':
             self.outs += 1
             print("아웃!", end=' ')
-        if(self.result == 'hits'):
+        if self.result == 'hits':
             self.hits += 1
             print("안타!", end=' ')
 
 
 def main():
     while(True):
-        print("신나는 야구 시합\n1. 데이터 입력\n2. 데이터 출력\n0. 종료")
+        print("신나는 야구 시합\n1. 데이터 입력\n2. 데이터 출력\n3. 시합 시작\n0. 종료")
         # print("메뉴선택 ( 1 - 2) ", end=' ')
         sys.stdout.write("\n 메뉴 선택 (1 - 2) ")
         choose = int(sys.stdin.readline().rstrip())
 
-        '''
+
         #sample player data
-        team1 = Team("Team01")
+        team1 = Team("DOGS")
         team1.player_list.append( Player(0,"지성",0.123) )
         team1.player_list.append( Player(1,"소지섭",0.323) )
         team1.player_list.append( Player(2,"조승우",0.989) )
-        team2 = Team("Team02")
+        team2 = Team("CATS")
         team2.player_list.append( Player(0,"공유",0.987) )
         team2.player_list.append( Player(0,"박보검",0.1111) )
         team2.player_list.append( Player(0,"노태규",0.922) )
-        '''
+
         if choose == 1:
             # print("1팀의 이름을 입력하세요> ")
 
@@ -102,27 +149,18 @@ def main():
             except UnboundLocalError:
                 print("데이터가 없습니다.\n")
                 continue
+        elif choose == 3:
+            game = Game(team1, team2)
+            game.startGame()
         elif choose == 0:
             print('종료')
             break
         else:
             print("잘못 입력하셨습니다. 0-2 중에서 입력해주세요.")
 
-'''
-    round = Game()
-    while(True):
-        round.throw()
-        round.updateResult()
-        if(round.outs == 3): #3아웃이면 전체 안타수 출력 후 경기 종료
-            round.display()
-            print('최종 안타수: {}\n GAME OVER'.format(round.hits))
-            break;
-        if(round.result == 'hits' or round.result == 'outs'):
-            print('다음 타자가 타석에 입장했습니다.')
-            round.strike = 0
-            round.ball = 0
-        round.display()
-'''
+
+
+
 
 
 if __name__ == "__main__":
